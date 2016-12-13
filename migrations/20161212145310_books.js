f7a4ac03df7c75f091ce777e6f869ff74be5b886
Paul E Lederer <plederer93@gmail.com>
2016-12-13 10:13:17 -0600

    added comments

--- migrations/20161212145310_books.js
+++ migrations/20161212145310_books.js
@@ -7,12 +7,19 @@ exports.up = (knex) =>
 {
   return knex.schema.createTable('books', (table) =>
   {
+    // id
     table.increments();
+    // title
     table.string('title').notNullable().defaultTo('');
+    // author
     table.string('author').notNullable().defaultTo('');
+    // genre
     table.string('genre').notNullable().defaultTo('');
+    // description
     table.text('description').notNullable().defaultTo('');
+    // cover_url
     table.text('cover_url').notNullable().defaultTo('');
+    // created_at and updated_at
     table.timestamps(true, true);
   });
 };
